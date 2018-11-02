4d3418ddb2fd973df5adf0566a7e3c423084e08b
yangtaoqibao <1821470255@qq.com>
2018-11-02 08:33:14 +0800

    qs

---
+++ abc.php
@@ -0,0 +1,46 @@
+<?php
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+	<center>
+		<h5>斐波那契数列: 1  1  2  3  5  8 ...</h5>
+
+		<p>前第一项<input type="text" name="first"></p>+
+		<p>前第二项<input type="text" name="second"></p>=
+		<p>当前项<input type="text" name="dang"></p>
+
+
+
+	</center>
+</body>
+</html>
